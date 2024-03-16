e4e4d20b8fbec84a7ce6aaf4f9488d2dcc02e72e
gahyunkim <mapledt001@naver.com>
2024-03-16 23:54:26 +0900

    study_nomad_dart

--- Classes/constructors.dart
+++ Classes/constructors.dart
@@ -1,8 +1,10 @@
 class Player {
   // 아래처럼 final을 붙여주면, name 의 값을 변경할 수 없음
   // final String name = 'nico';
+
   late final String name; // late는 변수들의 값을 나중에 받아올거라는걸 의미함
   late int xp;
+
   // 변수 선언은 여기서 했지만, 값은 나중에 받아온다는 것
 
 // constructor method의 이름은 class의 이름과 같아야 함
@@ -12,6 +14,7 @@ class Player {
   // }
 
   // 근데 위의 내용이 너무 반복적이라고 생각됨 따라서 아래와 같이 더 짧은 Constructor를 만들 수 있음
+
   Player(this.name, this.xp);
 
   void sayHello() {
@@ -23,6 +26,7 @@ class Player {
 // 하지만, 클래스 내에서는 명시해 줘야 함
 void main() {
   var player = Player("nico", 1500);
+
   // print(player.name);
   // player.name = 'lalala';
   // print(player.name);
